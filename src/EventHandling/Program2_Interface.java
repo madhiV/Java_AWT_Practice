da7dc51ebcc2ed869197c82241e4daf02b683cf4
madhiV <72497080+madhiV@users.noreply.github.com>
2021-07-13 13:36:03 +0530

    Graphics(AWT)

--- src/EventHandling/Program2_Interface.java
+++ src/EventHandling/Program2_Interface.java
@@ -1,4 +1,21 @@
 package EventHandling;
-
+interface M{
+    public void register();
+}
+class B implements M{
+    @Override
+    public void register(){
+        System.out.println("B registration !!!");
+    }
+}
 public class Program2_Interface {
+    public static void main(String[] args){
+        new B().register();
+        new M(){
+            @Override
+            public void register(){
+                System.out.println("Anonumous registration !!!");
+            }
+        }.register();
+    }
 }
